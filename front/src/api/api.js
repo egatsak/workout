3edb87ac9e92b03068aff3bfd24db48da8c528e4
Eugene Gatsak <greenglaz@gmail.com>
2023-01-03 14:59:07 +0400

    fix: fix backend package.json scripts

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL: "https://workout-app.up.railway.app/api",
   headers: {
     "Content-Type": "application/json"
   }
